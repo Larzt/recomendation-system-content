71d3f6507be4a615149470032b1d6a50b88ff1e2
Alejandro <alejandrorodriguezalcala1@gmail.com>
2025-10-24 09:49:57 +0100

    Hello from Vue

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import {createI18n} from 'vue-i18n'
+import PhosphorIcons from '@phosphor-icons/vue'
 import App from './App.vue'
 
+import es from '@/locales/es.json'
+import en from '@/locales/en.json'
+
 const app = createApp(App)
+const pinia = createPinia();
+const i18n = createI18n({
+  locale: 'en',
+  fallbackLocale: 'en',
+  messages: { en, es }
+})
 
-app.use(createPinia())
 
+app.use(pinia);
+app.use(PhosphorIcons);
+app.use(i18n);
 app.mount('#app')
---
+++ src/types/global.d.ts
@@ -0,0 +1,5 @@
+declare global {
+  // Here goes types and interfaces
+}
+
+export {}
